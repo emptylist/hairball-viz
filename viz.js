975153ad1c115f059dfd35f80aa48da2252d9d54
James Crooks <crooks1379@gmail.com>
2015-01-06 22:48:06 -0600

    forces working properly

--- viz.js
+++ viz.js
@@ -20,25 +20,41 @@ var Vector = {
   }
 };
 
+var Bond = {
+  create: function(vertex, k) {
+    var self = Object.create(this);
+    self.vertex = vertex;
+    self.k = k;
+
+    return self;
+  }
+}
+
 var Origin = Vector.create(300, 300);
-var stiffness = 1;
-var coeffElec = 500;
-var coeffFriction = 0.8;
+var stiffness = 5;
+var bondDist = 100;
+var coeffElec = 10;
+var coeffFriction = 0.2;
 var VertexTable = [];
 
 var Vertex = {
-  create: function(posVec, ts, bonds) {
+  create: function(posVec, ts) {
     var self = Object.create(this);
     self.pos = posVec;
     self.dt = ts;
-    self.bonds = bonds;
+    self.bonds = [];
     self.velocity = Vector.create(0,0);
     self.spring = Vector.create(0,0);
     self.elec = Vector.create(0,0);
     self.bonded = Vector.create(0,0);
+    self.bondedTemp = Vector.create(0,0);
     self.friction = Vector.create(0,0);
+    self.stepVel = Vector.create(0,0);
 
     return self;
+  },
+  addBond: function(bond) {
+    this.bonds.push(bond);
   },
   x: function() {
     return this.pos.x;
@@ -58,34 +74,50 @@ var Vertex = {
     for (i=0; i < VertexTable.length; i++) {
       var r = Math.sqrt(Math.pow(this.x() - VertexTable[i].x(), 2) +
                         Math.pow(this.y() - VertexTable[i].y(), 2));
+      r *= 0.5;
       if (r != 0) {
-        this.elec.x += -1 * coeffElec * (this.x() - VertexTable[i].x()) / r;
-        console.log(i + " " + this.elec.x);
-        this.elec.y += -1 * coeffElec * (this.y() - VertexTable[i].y()) / r;
+        this.elec.x += -1 * coeffElec * (this.x() - VertexTable[i].x()) / Math.pow(r,2);
+        this.elec.y += -1 * coeffElec * (this.y() - VertexTable[i].y()) / Math.pow(r,2);
       }
     }
     this.elec.multConst(this.dt);
   },
   updateBonded: function() {
+    this.bonded.setVec(0,0);
+    var i;
+    var sgn;
+    for (i=0; i<this.bonds.length; i++) {
+      var k = this.bonds[i].k;
+      var distance = Math.sqrt(
+                      Math.pow(this.pos.x - this.bonds[i].vertex.pos.x, 2) +
+                      Math.pow(this.pos.y - this.bonds[i].vertex.pos.y, 2));
+      var magnitude = 10 * k * -1 * (distance - ((1/k) * bondDist));
+      this.bondedTemp.setVec(
+        magnitude * ((this.pos.x - this.bonds[i].vertex.pos.x) / distance),
+        magnitude * ((this.pos.y - this.bonds[i].vertex.pos.y) / distance));
+      this.bonded.addVec(this.bondedTemp);
+    }
+    this.bonded.multConst(this.dt);
   },
   updateFriction: function() {
     this.friction.setVec(-1 * this.velocity.x, -1 * this.velocity.y);
     this.friction.multConst(coeffFriction);
-    this.friction.multConst(this.dt);
   },
   update: function() {
-    var stepVel = Vector.create(this.velocity.x, this.velocity.y);
-    stepVel.multConst(this.dt);
-    this.pos.addVec(stepVel);
     this.updateSpring();
     this.updateElec();
     this.updateBonded();
-    this.updateFriction();
+
     this.velocity.addVec(this.spring);
     this.velocity.addVec(this.elec);
+    this.velocity.addVec(this.bonded);
+
+    this.updateFriction();
     this.velocity.addVec(this.friction);
-    //this.velocity.addVec(this.strong());
-    //this.velocity.addVec(this.bonded());
+
+    this.stepVel.setVec(this.velocity.x, this.velocity.y);
+    this.stepVel.multConst(this.dt);
+    this.pos.addVec(this.stepVel);
   }
 };
 
@@ -96,11 +128,21 @@ VertexTable.push(v1);
 VertexTable.push(v2);
 VertexTable.push(v3);
 
+v1.addBond(Bond.create(v2, 0.5));
+v1.addBond(Bond.create(v3, 1));
+v2.addBond(Bond.create(v1, 0.5));
+v2.addBond(Bond.create(v3, 1));
+v3.addBond(Bond.create(v1, 1));
+v3.addBond(Bond.create(v2, 1));
+
 var svg = d3.select("svg");
 
 var c1 = svg.select("#c1");
 var c2 = svg.select("#c2");
 var c3 = svg.select("#c3");
+var b1 = svg.select("#b1");
+var b2 = svg.select("#b2");
+var b3 = svg.select("#b3");
 
 c1.attr("cx", v1.x())
   .attr("cy", v1.y());
@@ -110,6 +152,21 @@ c2.attr("cx", v2.x())
 
 c3.attr("cx", v3.x())
   .attr("cy", v3.y());
+
+b1.attr("x1", v1.x())
+  .attr("y1", v1.x())
+  .attr("x2", v2.x())
+  .attr("y2", v2.y());
+
+b2.attr("x1", v2.x())
+  .attr("y1", v2.y())
+  .attr("x2", v3.x())
+  .attr("y2", v3.y());
+
+b3.attr("x1", v3.x())
+  .attr("y1", v3.y())
+  .attr("x2", v1.x())
+  .attr("y2", v1.y());
 
 var counter = 0;
 
@@ -128,10 +185,26 @@ function update() {
   c3
     .attr("cx", v3.x())
     .attr("cy", v3.y());
+
+  b1.attr("x1", v1.x())
+    .attr("y1", v1.y())
+    .attr("x2", v2.x())
+    .attr("y2", v2.y());
+
+  b2.attr("x1", v2.x())
+    .attr("y1", v2.y())
+    .attr("x2", v3.x())
+    .attr("y2", v3.y());
+
+  b3.attr("x1", v3.x())
+    .attr("y1", v3.y())
+    .attr("x2", v1.x())
+    .attr("y2", v1.y());
+
   //counter = 0;
   //}
 
 };
 update();
-setInterval(update, 30);
+setInterval(update, 50);
 
